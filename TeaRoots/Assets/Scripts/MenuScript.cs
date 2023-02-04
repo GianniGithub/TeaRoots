d5e4aa85c0152b11d67051c649649e7bab55858a
gianni <gianni@posteo.de>
2023-02-04 10:47:58 +0100

    Basic Scenen

---
+++ TeaRoots/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+	public void ChangeScene(string Name)
+	{
+		SceneManager.LoadScene(Name);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+
+	}
+}
